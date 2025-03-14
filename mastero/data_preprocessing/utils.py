3d02c9b5caa5e06418e9dfafe26db14596cbeae1
Leon Debatin <96688757+KIThubber@users.noreply.github.com>
2025-03-14 14:03:07 +0100

    refactored structure

---
+++ mastero/data_preprocessing/utils.py
@@ -0,0 +1,33 @@
+from sklearn.model_selection import train_test_split
+
+
+
+def basic_information(df):
+    print('Total NA:',df.isna().sum().sum())
+    print('Imbalance:',df['target'].value_counts())
+    print('Dtyoes:',df.dtypes)
+    print('duplicates', df.duplicated().sum())
+
+def get_train_test_indices(df):
+    train_indices = []
+    test_indices = []
+    for i in range(1,31):
+        train, test = train_test_split(df.index, test_size=0.3, random_state=i, stratify=df['target'])
+        train_indices.append(train.to_list())
+        test_indices.append(test.to_list())
+    
+    return train_indices, test_indices
+
+
+def info_dict(name, df, categoricals):
+    info = {}
+    info['name'] = name
+    info['n_samples'] = df.shape[0]
+    info['n_features'] = df.shape[1] - 1
+    info['imbalance'] = df['target'].value_counts(ascending=True)[1] / df.shape[0]
+    info['categoricals'] = categoricals
+    info['n_categoricals'] = len(categoricals)
+    info['train_indices'], info['test_indices'] = get_train_test_indices(df)
+    
+    return info
+    
